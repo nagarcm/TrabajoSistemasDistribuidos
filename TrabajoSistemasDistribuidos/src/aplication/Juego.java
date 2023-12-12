d08e7530a972cc6afefab01baa5e18978120019c
nagarcm <nagarcm@CCTS1-28.unirioja.loc>
2023-12-12 19:22:02 +0100

    He quitado el Throw Exception

--- TrabajoSistemasDistribuidos/src/aplication/Juego.java
+++ TrabajoSistemasDistribuidos/src/aplication/Juego.java
@@ -10,7 +10,7 @@ import java.io.*;
 
 import java.util.*;
 public class Juego {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
         InterfazTextual it = new InterfazTextual(new Game());
         it.startGame();
     }
